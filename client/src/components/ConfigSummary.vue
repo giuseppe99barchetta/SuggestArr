75b91c1a6e2c3fa870b792495fda4179b1c7bf3d
Giuseppe Barchetta <giuseppebarchetta8@gmail.com>
2024-11-05 10:27:42 +0100

    [FIX] Reading of new version available.

--- client/src/components/ConfigSummary.vue
+++ client/src/components/ConfigSummary.vue
@@ -136,13 +136,21 @@ export default {
             try {
                 const response = await axios.get('https://api.github.com/repos/giuseppe99barchetta/SuggestArr/releases/latest');
                 this.latestVersion = response.data.tag_name;
-                this.isUpdateAvailable = this.currentVersion.replace('v', '') < this.latestVersion.replace('v', '');
-                if (this.isUpdateAvailable){
+            
+                const [currentMajor, currentMinor, currentPatch] = this.currentVersion.replace('v', '').split('.').map(Number);
+                const [latestMajor, latestMinor, latestPatch] = this.latestVersion.replace('v', '').split('.').map(Number);
+            
+                this.isUpdateAvailable = 
+                    latestMajor > currentMajor ||
+                    (latestMajor === currentMajor && latestMinor > currentMinor) ||
+                    (latestMajor === currentMajor && latestMinor === currentMinor && latestPatch > currentPatch);
+            
+                if (this.isUpdateAvailable) {
                     this.$toast.open({
-                            message: 'New version of SuggestArr available!',
-                            pauseOnHover: true,
-                            duration:5000
-                        })
+                        message: 'New version of SuggestArr available!',
+                        pauseOnHover: true,
+                        duration: 5000
+                    });
                 }
             } catch (error) {
                 console.error('Failed to check for updates:', error);
