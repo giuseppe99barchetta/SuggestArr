8319db14d42260a32b9f31fadc7bf55a4cb203c3
Giuseppe Barchetta <giuseppebarchetta8@gmail.com>
2025-02-07 15:22:06 +0100

    [FEATURE] Update database password key in test configuration for consistency

--- api_service/test/test_config.py
+++ api_service/test/test_config.py
@@ -42,7 +42,7 @@ class TestConfig(unittest.TestCase):
         "DB_HOST": "localhost",
         "DB_PORT": "5432",
         "DB_USER": "postgres",
-        "DB_PASS": "password",
+        "DB_PASSWORD": "password",
         "DB_NAME": "suggestarr",
     }
 
