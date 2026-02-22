9c104ad6259d5cf09a1f5d678d6394de91265d81
unraid-iac <erik@forsberg.li>
2026-02-22 08:32:05 +0100

    fix subpath crashes

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,11 +3,12 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   transpileDependencies: [],
-  
+
   chainWebpack: config => {
     config.resolve.alias.set('@', path.resolve(__dirname, 'src'))
-    
+
     config.plugin('define').tap(args => {
       const pkg = require('./package.json')
       args[0]['process.env.VUE_APP_VERSION'] = JSON.stringify(pkg.version)
